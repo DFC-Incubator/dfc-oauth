7794ba8949c407efc71d783250d7f944cb7c59c9
michael-conway <michael_conway@unc.edu>
2016-12-15 14:51:29 -0500

    #1 initial pom at least runs

--- src/main/java/org/irods/jargon/oauth/utils/OauthVersion.java
+++ src/main/java/org/irods/jargon/oauth/utils/OauthVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.oauth.utils;
 public final class OauthVersion {
  public static String VERSION="1.0.0-SNAPSHOT";
- public static String BUILD_TIME="2016-12-13T20:35:30Z";
+ public static String BUILD_TIME="2016-12-15T19:50:59Z";
 }
